67a410da7f4e73464e0f4727301fb044087bbb33
Vladimir Strelec <86953981+Vladimir-Strelec@users.noreply.github.com>
2022-09-20 22:31:01 +0300

    Fix frontend

--- caffe/common/view_mixin.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-
-class RedirectToCatalog:
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('catalog')
-        return super().dispatch(request, *args, **kwargs)
